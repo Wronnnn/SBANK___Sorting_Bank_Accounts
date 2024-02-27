f431bd630f51437d68a351fe2f61b5cb577de53e
WronM <skowronmaciej@protonmail.com>
2024-02-27 10:23:41 +0100

    Powinno działać ale nie działa i mam to gdzieś

--- main.cpp
+++ main.cpp
@@ -1,41 +1,66 @@
 #include <iostream>
 
 void merge(std::string array[], int left, int mid, int right);//to i
-void mergeSort(std::string array[], int begin, int end);//to jest algorytm mergesort zaporzyczony z internetu
+void mergeSort(std::string array[], int begin, int end);////////to jest algorytm mergesort zaporzyczony z internetu
 
 int main() {
-    int numOfCases, numOfTests, numOfRep, temp;
-    auto *resoult = new std::string;
+    int numOfCases, numOfTests, numOfRep;
+    std::string temp;
+//    auto *resoult = new std::string;
     std::cin>>numOfTests;
+    int arrayForLenghts[numOfTests];
+    std::string **arrayForClients = new std::string*[numOfTests];
 
     for(int xx=0;xx<numOfTests;xx++){//dla każdego testu
-        resoult->clear();
+//        resoult->clear();
         numOfRep=1;
         std::cin>>numOfCases;
-        auto *arrayForClients = new std::string[numOfCases];
+        arrayForLenghts[xx] = numOfCases;
+        arrayForClients[xx] = new std::string[numOfCases];
 //        std::string arrayForClients[numOfCases];
         std::fflush(stdin);
         for(int i=0;i<numOfCases;i++){//pobieranie numerow
-            std::getline(std::cin, arrayForClients[i]);
+            std::getline(std::cin, arrayForClients[xx][i]);
         }
+        if(xx+1 != numOfTests){
+            std::getline(std::cin, temp);//przyjmuje jedna pusta linie
+            std::fflush(stdin);
+        }
+        mergeSort(arrayForClients[xx], 0, numOfCases - 1);//sortowanie listy
 
-        mergeSort(arrayForClients, 0, numOfCases - 1);//sortowanie listy
-
-        for(int i=1;i<numOfCases;i++){//wyswietlanie pojedynczo w posortowanej kolejnosci numerow kont i ilosci ich powtorzen
-            if(arrayForClients[i - 1] == arrayForClients[i]){
+//        for(int i=1;i<numOfCases;i++){//wyswietlanie pojedynczo w posortowanej kolejnosci numerow kont i ilosci ich powtorzen
+//            if(arrayForClients[i - 1] == arrayForClients[i]){
+//                numOfRep++;
+//            } else {
+////                resoult->append(arrayForClients[i-1] + ' ' + '\n');
+//                std::cout<<arrayForClients[i-1]<<' '<<numOfRep<<'\n';
+//                numOfRep=1;
+//            }
+//        }
+//        resoult->append(arrayForClients[numOfCases-1] + ' ' + '\n');
+//        std::cout<<arrayForClients[numOfCases-1]<<' '<<numOfRep<<'\n';//wyswietlenie numeru konta i liczby powtorzen tylko ze dla ostatniego numeru
+////        std::cout<<resoult<<'\n';
+//        if(xx != numOfTests-1){
+//            std::cin>>temp;
+//        }
+    }
+    for(int xx=0;xx<numOfTests;xx++){
+        numOfRep = 1;
+        for(int i=1;i<arrayForLenghts[xx];i++){//wyswietlanie pojedynczo w posortowanej kolejnosci numerow kont i ilosci ich powtorzen
+            if(arrayForClients[xx][i - 1] == arrayForClients[xx][i]){
                 numOfRep++;
             } else {
 //                resoult->append(arrayForClients[i-1] + ' ' + '\n');
-                std::cout<<arrayForClients[i-1]<<' '<<numOfRep<<'\n';
+                std::cout<<arrayForClients[xx][i-1]<<' '<<numOfRep<<'\n';
                 numOfRep=1;
             }
         }
-//        resoult->append(arrayForClients[numOfCases-1] + ' ' + '\n');
-        std::cout<<arrayForClients[numOfCases-1]<<' '<<numOfRep<<'\n';//wyswietlenie numeru konta i liczby powtorzen tylko ze dla ostatniego numeru
-        std::cout<<resoult<<'\n';
-        if(xx != numOfTests-1){
-            std::cin>>temp;
-        }
+        std::cout<<arrayForClients[xx][numOfCases-1]<<' '<<numOfRep<<'\n';//wyswietlenie numeru konta i liczby powtorzen tylko ze dla ostatniego numeru
+//        std::cout<<resoult<<'\n';
+//        if(xx != numOfTests-1){
+//            std::cin>>temp;
+//        }
+        std::cout<<'\n';
     }
     return 0;
 }
